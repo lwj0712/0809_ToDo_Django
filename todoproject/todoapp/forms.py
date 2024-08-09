8e8d3e11f65c30f040890b6f86674ea496f8e6b3
lwj0712 <dldnjswot@naver.com>
2024-08-09 14:16:40 +0900

    first commit

---
+++ todoproject/todoapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task # task 모델과 해당 폼을 연결
+        fields = ['title', 'description', 'due_date', 'list'] # 폼에 포함될 모델 필드를 지정
+        widgets = {
+            'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        } # due_date 필드에 대한 위젯을 지정
---
+++ todoproject/todoapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class List(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    list = models.ForeignKey(List, on_delete=models.CASCADE, related_name='tasks')
+
+    def __str__(self):
+        return self.title
