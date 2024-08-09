6c11704d1bbe68005fba696abbf7009f76fbddde
lwj0712 <dldnjswot@naver.com>
2024-08-09 14:42:49 +0900

    update task_list

--- todoproject/todoapp/forms.py
+++ todoproject/todoapp/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from .models import Task
+from .models import Task, List
 
 class TaskForm(forms.ModelForm):
+    list = forms.ModelChoiceField(queryset=List.objects.all(), required=False)
+
     class Meta:
         model = Task # task 모델과 해당 폼을 연결
         fields = ['title', 'description', 'due_date', 'list'] # 폼에 포함될 모델 필드를 지정
--- todoproject/todoapp/views.py
+++ todoproject/todoapp/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Task
+from django.shortcuts import render, redirect, get_object_or_404 # redirect 함수는 다른 URL로 이동하는 함수
+from .models import Task, List
 from .forms import TaskForm
 
 def task_list(request):
     tasks = Task.objects.all().order_by('-created_date')
-    return render(request, 'todoapp/task_list.html', {'tasks': tasks})
+    list = List.objects.all()
+    return render(request, 'todoapp/task_list.html', {'tasks': tasks, 'lists': list}) # task_list.html 템플릿을 렌더링한다.
 
 def add_task(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('task_list')
+    if request.method == 'POST': # 폼이 제출 되었을때 POST 요청을 처리한다. 처리가됨
+        form = TaskForm(request.POST) # POST 요청으로 전달된 데이터를 사용해 TaskForm 인스턴스를 생성한다.
+        if form.is_valid(): # form의 데이터가 유효한지 검사 -> 모델/제약조건을 기반으로 자동 유효성 검증을 수행한다.
+            form.save() # form의 데이터를 데이터베이스에 저장한다.
+            return redirect('task_list') # task_list 뷰로 이동한다.
     else:
-        form = TaskForm()
-    return render(request, 'todoapp/add_task.html', {'form': form})
+        form = TaskForm() # GET 요청일때는 빈 TaskForm 인스턴스를 생성한다.
+    lists = List.objects.all()
+    return render(request, 'todoapp/add_task.html', {'form': form, 'lists':lists}) # 폼을 포함한 add_task.html 템플릿을 렌더링한다.
 
 def edit_task(request, task_id):
     task = get_object_or_404(Task, id=task_id)
